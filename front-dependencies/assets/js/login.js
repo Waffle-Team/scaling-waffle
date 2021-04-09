5c9046856c739447aedaa26b9a86e45e6e7b3cf1
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2021-04-09 17:08:43 -0300

    mudança nos formularios

---
+++ front-dependencies/assets/js/login.js
@@ -0,0 +1,12 @@
+function login(pass, user){
+    //Implementar
+    var session_token;
+    session_token = true;
+    return session_token;
+}
+$(document).ready(function() {
+    var user = $('#login').val();
+    var pass = $('#senha').val();
+
+
+});
---
+++ front-dependencies/assets/js/register.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $("#bt-registrar").click(function(){
+        var nome = $('#nome').val();
+        var sobrenome = $('#sobrenome').val();
+        var nickname = $('#nick').val();
+        var email = $('#email').val();
+        var telefone = $('#telefone').val();
+        var senha = $('#senha').val();
+        var confirmarsenha = $('#confirmarsenha').val();
+
+        
+
+
+    });
+
+
+
+});
