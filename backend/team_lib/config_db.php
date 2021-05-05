6f900ab85df9021fc3a9c7745355a341d73ad146
CriminalShrimp <aleisterlima@yahoo.com.br>
2021-05-05 17:18:51 -0300

    configdb
    conexcao

--- backend/team_lib/config_db.php
+++ backend/team_lib/config_db.php
@@ -1,6 +1,8 @@
 <!--usar para configurações do banco de dados -->
 <!-- utilizar esteganografia -->
 <?php
-    $php
-
-?>
+define('HOST','127.0.0.1');//Porta conectada
+define('USUARIO','root');//Usuario do Bando de Dados
+define('SENHA','');//Senha VER COMO FAZER COM ESTEGANOGRAFIA
+define('DB', 'waffle');//Nome do Banco de Dados
+$conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die('Nao foi possivel conectar');
