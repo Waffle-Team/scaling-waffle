75516728bc3aa36e7ae102df0d26f200ce359ea2
CriminalShrimp <aleisterlima@yahoo.com.br>
2021-05-06 02:51:32 -0300

    ultiams att
    n ta funcionando devido a um erro q msqli com parametros, pra adiantar um pouco fiz a base do resto

--- backend/team_lib/config_db.php
+++ backend/team_lib/config_db.php
@@ -1,8 +1,27 @@
 <!--usar para configurações do banco de dados -->
 <!-- utilizar esteganografia -->
 <?php
-define('HOST','127.0.0.1');//Porta conectada
+
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'waffle');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+echo "pica";
+/*define('HOST','localhost');//Porta conectada
 define('USUARIO','root');//Usuario do Bando de Dados
 define('SENHA','');//Senha VER COMO FAZER COM ESTEGANOGRAFIA
 define('DB', 'waffle');//Nome do Banco de Dados
-$conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die('Nao foi possivel conectar');
+$conexao = mysqli_connect(HOST,USUARIO,SENHA,DB);*/
+
+/*if(!$conexao){
+   echo "errno: " . mysqli_connect_errno() . PHP_EOL;
+   echo "error: " . mysqli_connect_error() . PHP_EOL;
+}
+echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+ 
+function falar(){
+   echo"aaaaaa";
+   return;
+}*/
