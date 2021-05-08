e3fba3848eb0b1b27aa5b528b19f957205edccb0
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2021-05-07 22:37:21 -0300

    Revert "Merge branch 'Stable' into mail"
    This reverts commit f8be9ff296b0a803d6bc421fbaefbc2c4dfcd7ca, reversing
    changes made to ccda14fe11bfa94c2fc4d73a8fafce5a1a99b32c.

--- backend/team_lib/config_db.php
+++ backend/team_lib/config_db.php
@@ -2,18 +2,20 @@
 <!-- utilizar esteganografia -->
 <?php
 
-define('HOST', '127.0.0.1');//IP do maquina
-define('USUARIO', 'root');//Nome usuario
-define('SENHA', '');//Senha 
-define('DB', 'waffle');//Nome do banco
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'waffle');
+
+
+echo "pica\n";
 
 function conecta_db(){
-   $con = new mysqli(HOST, USUARIO, SENHA, DB, 3306);
+   return $con = new mysqli(HOST, USUARIO, SENHA, DB, 3306);
    // Check connection
    if ($con->connect_error) {
       die("ERROO DE CONEÇÃO:" . $con->connect_error);
   }
   echo "CONECTADO AO DB\n";
-  return $con;
 }
 
