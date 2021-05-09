f2649c725b748218cf4d0f598aad6ed4c7efc119
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2021-05-09 19:42:41 -0300

    Update verificar.php

--- backend/verificar.php
+++ backend/verificar.php
@@ -2,17 +2,25 @@
 
 require_once 'team_lib/functions.php';
 
+
 // pega as duas variaveis da url
 $codigo = $_POST['codigo'];
 $apelido = $_POST['apelido'];
 $JsonReturn = new stdClass();
 
-if(isset($codigo)){
-    $dados = pesquisaUsuario($apelido); //pega as informacoes na DB pelo functions.php a partir do apelido
-    $chavedaconta = codigoverificacao($dados['nome'], $dados['sobrenome'], $dados['email'], $dados['telefone']); //recria o codigo a partir das informacoes
-    if ($chavedaconta = $codigo) {
-        validaConta($apelido); //se os codigo da url bate com o codigo feito com os dados da DB, altera na DB confirmado para 1
-    }
+
+$dados = pesquisaUsuario($apelido); //pega as informacoes na DB pelo functions.php a partir do apelido
+
+if(!$dados){
+    $JsonReturn->sucess = FALSE;
+    $JsonReturn->msg = 'Não foi possivel verificar conta';
+    print(json_encode($JsonReturn));//retorno
+    exit();
+}
+
+$chavedaconta = codigoverificacao($dados['nome'], $dados['sobrenome'], $dados['email'], $dados['telefone']); //recria o codigo a partir das informacoes
+if ($chavedaconta = $codigo) {
+    validaConta($apelido); //se os codigo da url bate com o codigo feito com os dados da DB, altera na DB confirmado para 1
     $JsonReturn->sucess = TRUE;
     $JsonReturn->msg = 'Email confimado com sucesso, agora é só começar a usar';
     print(json_encode($JsonReturn));//retorno
