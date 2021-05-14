3f0f26b6675e2c23d5f839fe42056c8c4ea740a9
CriminalShrimp <aleisterlima@yahoo.com.br>
2021-05-14 18:39:26 -0300

    configdb
    ta feito com estenografia
    
    Co-Authored-By: Maideh <51738019+Maideh@users.noreply.github.com>
    Co-Authored-By: Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>

--- backend/team_lib/config_db.php
+++ backend/team_lib/config_db.php
@@ -1,10 +1,19 @@
 <?php
+<<<<<<< Updated upstream
 // <!--usar para configurações do banco de dados -->
 // <!-- utilizar esteganografia -->
 
 define('HOST', '127.0.0.1');//IP do maquina
 define('USUARIO', 'root');//Nome usuario
 define('SENHA', '');//Senha 
+=======
+include('../lib/esteganografia/decrypt.php');
+
+
+define('HOST', '127.0.0.1');//IP do maquina
+define('USUARIO', 'waffle');//Nome usuario
+define('SENHA', decript_estegano('../lib/esteganografia/result633.png'));//Senha
+>>>>>>> Stashed changes
 define('DB', 'waffle');//Nome do banco
 
 function conecta_db(){
@@ -12,6 +21,7 @@ function conecta_db(){
    // Check connection
    if ($con->connect_error) {
       die("ERRO DE CONECÇÃO:" . $con->connect_error);
+      return FALSE;
   }
 //   echo "CONECTADO AO DB\n";
   return $con;
