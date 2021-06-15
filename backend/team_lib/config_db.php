870fd4a585ca8e8af257eeda261ee58ead1bd807
CriminalShrimp <aleisterlima@yahoo.com.br>
2021-06-15 20:47:43 -0300

    ta certo as parada no banco
    Co-Authored-By: Spl3F <66442461+Spl3F@users.noreply.github.com>
    Co-Authored-By: Gustavo Schmidt <53221408+GustaSchmidt@users.noreply.github.com>

--- backend/team_lib/config_db.php
+++ backend/team_lib/config_db.php
@@ -11,9 +11,10 @@ function conecta_db(){
    $con = new mysqli(HOST, USUARIO, SENHA, DB, 3306);
    // Check connection
    if ($con->connect_error) {
-      die("ERRO DE CONECÇÃO:" . $con->connect_error);
-      return FALSE;
-  }
-echo "CONECTADO AO DB\n";
-  return $con;
+        die("ERRO DE CONECÇÃO:" . $con->connect_error);
+        return FALSE;
+    }
+    //echo "CONECTADO AO DB\n";
+    return $con;
 }
+?>
