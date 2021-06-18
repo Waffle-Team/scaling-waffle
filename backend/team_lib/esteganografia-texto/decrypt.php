cd0c900e2af4ed5e0154d4d7b2ee9c3f208cf084
Maideh <gwrbk@hotmail.com>
2021-06-18 17:34:04 -0300

    nenhum arquivo random

--- backend/team_lib/esteganografia-texto/decrypt.php
+++ backend/team_lib/esteganografia-texto/decrypt.php
@@ -1,30 +1,39 @@
 <?php
+
+function decifrar($codigo){
     $resultado = fopen("resultado.txt", "w"); //codigo decifrado
     $script = fopen("script.txt", "r"); //o script base
-    $codigo = fopen("saida.txt", "r"); //script com o codigo
+    //$codigo = fopen("saida.txt", "r"); //script com o codigo
 
-    $linhascodigo = count(file("saida.txt"));
+    
+    $linhascodigo = count(file("saida.txt")); //conta as linhas do script com o codigo
 
-    for ($i = 0; $i < $linhascodigo; $i++) {
+    for ($i = 0; $i < $linhascodigo; $i++) { //deleta o primeiro caractere de todas as linhas
         fwrite($resultado, substr(fgets($codigo), 0, 1));
     }
 
     fclose($resultado);
-    $texto = file('resultado.txt'); 
+    $texto = file('resultado.txt');
     $resultado = fopen("resultado.txt", "w");
 
-    $linhas = count($texto);
+    $linhas = count($texto); //conta as linhas do script com o codigo
     unset($texto[$linhas - 1]); 
     
-    for ($i = 1; $i < $linhas - 1; $i++){
+
+    for ($i = 1; $i < $linhas - 1; $i++){ //deleta completamente a ultima linha
         $texto[$i] = substr($texto[$i], 1);
     }
-
-    fwrite($resultado, implode('', $texto));
     
+    fwrite($resultado, implode('', $texto));
+
+    $chavelimpa = file_get_contents("resultado.txt"); //le o conteúdo para uma variavel $chavelimpa
+
+    unlink("resultado.txt"); //deleta o arquivo com a chave
+
     fclose($codigo);
     fclose($script);
-    fclose($resultado);
 
+    return $chavelimpa;
+}
 
 ?>
