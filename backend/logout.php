ecacd84f347227fb0a9874b0b6d37fa3f9fdfb01
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2021-06-19 21:09:10 -0300

    Update logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -1,8 +1,16 @@
 <?php
 session_start();//Inicia area de codigo de seção
 session_unset();//deleta variaveis da sessão
+if (isset($_SERVER['HTTP_COOKIE'])) {//deleta cookies
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie){
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 session_destroy();//Destroi a seção
-
 header('location: ../');
 
 
